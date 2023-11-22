77a3f3cd2ef7e023fce8caf79d3a2cf69e991477
anhnv-nhsv <anhnv@nhsv.vn>
2023-11-22 17:35:29 +0700

    Add transactional annotation

--- src/main/java/au/com/vietnamesecolour/repos/RoleRepository.java
+++ src/main/java/au/com/vietnamesecolour/repos/RoleRepository.java
@@ -1,10 +1,14 @@
 package au.com.vietnamesecolour.repos;
 
 import au.com.vietnamesecolour.entity.Role;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findById(Integer id);
 }
--- src/main/java/au/com/vietnamesecolour/repos/TokenRepository.java
+++ src/main/java/au/com/vietnamesecolour/repos/TokenRepository.java
@@ -1,12 +1,16 @@
 package au.com.vietnamesecolour.repos;
 
 import au.com.vietnamesecolour.entity.Token;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
   @Query(value = """
