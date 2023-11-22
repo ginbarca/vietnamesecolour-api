b3d9808de3ed774f387fc4c5f9e05fb03e5b9ab1
Viet Anh Nguyen <nva1308@gmail.com>
2023-11-22 23:24:29 +0700

    Config FORBIDDEN/UNAUTHORIZED for request

--- src/main/java/au/com/vietnamesecolour/config/exception/CustomAccessDeniedHandler.java
+++ src/main/java/au/com/vietnamesecolour/config/exception/CustomAccessDeniedHandler.java
@@ -1,54 +1,57 @@
-//package io.bootify.my_app.config.exception;
-//
-//import io.bootify.my_app.utils.JsonUtils;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Component;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.nio.file.AccessDeniedException;
-//
-//@Component
-//public class CustomAccessDeniedHandler implements AccessDeniedHandler {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
-//
-//    private final LoggingProperties loggingProperties;
-//
-//    @Value("${spring.application.name}")
-//    private String serviceName;
-//
-//    @Autowired
-//    public CustomAccessDeniedHandler(LoggingProperties loggingProperties) {
-//        this.loggingProperties = loggingProperties;
-//    }
-//
-//    @Override
-//    public void handle(
-//            HttpServletRequest request, HttpServletResponse response, AccessDeniedException exc)
-//            throws IOException {
-//        logRequest(request, serviceName, loggingProperties);
-//        var auth = SecurityContextHolder.getContext().getAuthentication();
-//        if (auth != null) {
-//            LOGGER.warn(
-//                    String.format(
-//                            "User: %s attempted to access the protected URL: %s",
-//                            auth.getName(), request.getRequestURI()));
-//        }
-//        var errorCode = CommonErrorCode.FORBIDDEN;
-//        response.setStatus(errorCode.getHttpStatus().value());
-//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+package au.com.vietnamesecolour.config.exception;
+
+import au.com.vietnamesecolour.config.data.ResponseStatusCode;
+import au.com.vietnamesecolour.config.data.ResponseUtils;
+import au.com.vietnamesecolour.config.exception.CommonErrorCode;
+import au.com.vietnamesecolour.utils.JsonUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            LOGGER.warn(
+                    String.format(
+                            "User: %s attempted to access the protected URL: %s",
+                            auth.getName(), request.getRequestURI()));
+        }
+        ResponseStatusCode errorCode = ResponseStatusCode.FORBIDDEN;
+        response.setStatus(errorCode.getCode());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(
+                response.getOutputStream(),
+                ResponseUtils.error(errorCode.getCode(), errorCode.getDescription(), HttpStatus.FORBIDDEN)
+        );
 //        response
 //                .getWriter()
 //                .write(
 //                        JsonUtils.toJson(
 //                                ResponseUtils.getResponseDataError(
-//                                        errorCode.getCode(), errorCode.getMessage(), null, false)));
-//        logResponse(request, response, loggingProperties);
-//    }
-//}
+//                                        errorCode.getCode(), errorCode.getDescription(), null)));
+    }
+}
--- src/main/java/au/com/vietnamesecolour/config/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/au/com/vietnamesecolour/config/exception/CustomAuthenticationEntryPoint.java
@@ -1,45 +1,48 @@
-//package io.bootify.my_app.config.exception;
-//
-//import java.io.IOException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-//
-//  private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationEntryPoint.class);
-//
-//  private final LoggingProperties loggingProperties;
-//
-//  @Value("${spring.application.name}")
-//  private String serviceName;
-//
-//  @Autowired
-//  public CustomAuthenticationEntryPoint(LoggingProperties loggingProperties) {
-//    this.loggingProperties = loggingProperties;
-//  }
-//
-//  @Override
-//  public void commence(
-//      HttpServletRequest request,
-//      HttpServletResponse response,
-//      AuthenticationException authException)
-//      throws IOException {
-//    logRequest(request, serviceName, loggingProperties);
-//    LOGGER.warn("You need to login first in order to perform this action.");
-//    var errorCode = FORBIDDEN;
-//    response.setStatus(errorCode.getHttpStatus().value());
-//    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//    response
-//        .getWriter()
-//        .print(
-//            toJson(getResponseDataError(errorCode.getCode(), errorCode.getMessage(), null, false)));
-//    logResponse(request, response, loggingProperties);
-//  }
-//}
+package au.com.vietnamesecolour.config.exception;
+
+import java.io.IOException;
+
+import au.com.vietnamesecolour.config.data.ResponseStatusCode;
+import au.com.vietnamesecolour.config.data.ResponseUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import static au.com.vietnamesecolour.config.data.ResponseUtils.getResponseDataError;
+import static au.com.vietnamesecolour.utils.JsonUtils.toJson;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationEntryPoint.class);
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        LOGGER.warn("You need to login first in order to perform this action.");
+        ResponseStatusCode errorCode = ResponseStatusCode.UNAUTHORIZED;
+        response.setStatus(errorCode.getCode());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(
+                response.getOutputStream(),
+                ResponseUtils.error(errorCode.getCode(), errorCode.getDescription(), HttpStatus.UNAUTHORIZED)
+        );
+//        response.getWriter()
+//                .print(
+//                        toJson(
+//                                getResponseDataError(errorCode.getCode(), errorCode.getDescription(), null)
+//                        )
+//                );
+    }
+}
