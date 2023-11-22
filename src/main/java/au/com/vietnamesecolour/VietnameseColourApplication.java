a7581234188dc47426888e87cb99c866a6211c1a
anhnv-nhsv <anhnv@nhsv.vn>
2023-11-22 17:34:13 +0700

    Enable auditing

--- src/main/java/au/com/vietnamesecolour/VietnameseColourApplication.java
+++ src/main/java/au/com/vietnamesecolour/VietnameseColourApplication.java
@@ -2,9 +2,11 @@ package au.com.vietnamesecolour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class VietnameseColourApplication {
 
     public static void main(final String[] args) {
