8aacfc901f50ef4e551a03b2204248b2cb109114
anhnv-nhsv <anhnv@nhsv.vn>
2023-11-29 09:31:54 +0700

    Add dish image name and validate group/unit ID

--- src/main/java/au/com/vietnamesecolour/dto/DishInfoDTO.java
+++ src/main/java/au/com/vietnamesecolour/dto/DishInfoDTO.java
@@ -1,5 +1,6 @@
 package au.com.vietnamesecolour.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +16,12 @@ public class DishInfoDTO {
     private Float price;
     private String dishDescription;
     private String dishImagePath;
+    private String dishImageName;
     private String dishGroupName;
-//    @JsonIgnore
+    @NotNull(message = "Group ID must not be null")
     private Integer dishGroupId;
     private String unitName;
-//    @JsonIgnore
+    @NotNull(message = "Unit ID must not be null")
     private Integer unitId;
     private String createdDate;
     private String updatedDate;
