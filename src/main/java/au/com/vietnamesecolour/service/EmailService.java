d6eeedbc7800ad6acdac9277b24858ac1021a892
Viet Anh Nguyen <nva1308@gmail.com>
2023-12-06 10:39:42 +0700

    Add reset email template

--- src/main/java/au/com/vietnamesecolour/service/EmailService.java
+++ src/main/java/au/com/vietnamesecolour/service/EmailService.java
@@ -3,11 +3,17 @@ package au.com.vietnamesecolour.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 
 @Component
 @RequiredArgsConstructor
@@ -18,17 +24,38 @@ public class EmailService {
     public void sendPasswordResetVerificationEmail(String verifyUrl, String receiverEmail) throws MessagingException, UnsupportedEncodingException {
         String subject = "Password Reset Request Verification";
         String senderName = "Reset Password | VietnameseColour";
-        String mailContent = "<p> Hi,<br/> , </p>" +
-                "<p><b>You recently requested to reset your password,</b>" + "" +
-                "Please, follow the link below to complete the action.</p>" +
-                "<a href=\"" + verifyUrl + "\">Reset password</a>" +
-                "<p> Users Registration Portal Service";
+        String mailContent = getResetPwdHTMLContent().replace("{{verifyUrl}}", verifyUrl);
         MimeMessage message = mailSender.createMimeMessage();
-        var messageHelper = new MimeMessageHelper(message);
-        messageHelper.setFrom("no-reply@vietnamesecolour.com.au", senderName);
-        messageHelper.setTo(receiverEmail);
-        messageHelper.setSubject(subject);
-        messageHelper.setText(mailContent, true);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        FileSystemResource logo = new FileSystemResource(EmailService.class.getClassLoader().getResource("reset-pwd-template-email/images/artboard-26a2x.png").getFile());
+        FileSystemResource resetIcon = new FileSystemResource(EmailService.class.getClassLoader().getResource("reset-pwd-template-email/images/image-1.png").getFile());
+
+        helper.setFrom("no-reply@vietnamesecolour.com.au", senderName);
+        helper.setTo(receiverEmail);
+        helper.setSubject(subject);
+        helper.setText(mailContent, true);
+        helper.addInline("logo", logo);
+        helper.addInline("resetIcon", resetIcon);
         mailSender.send(message);
+    }
+
+    private String getResetPwdHTMLContent() {
+        try {
+            ClassLoader classLoader = EmailService.class.getClassLoader();
+            URL resource = classLoader.getResource("reset-pwd-template-email/index.html");
+
+            if (resource == null) {
+                throw new IllegalArgumentException("file is not found!");
+            } else {
+                return IOUtils.toString(new FileInputStream(resource.getFile()), "UTF-8");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(EmailService.class.getClassLoader().getResource("reset-pwd-template-email/images/artboard-26a2x.png").getFile());
+        System.out.println(file.getName());
     }
 }
